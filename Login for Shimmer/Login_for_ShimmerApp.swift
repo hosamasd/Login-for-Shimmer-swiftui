83d530b1b181672db38a370b821eb9cf4fe36d10
hosam <hosamelmalt@gmail.com>
2020-12-30 10:04:23 -0800

    Initial Commit

---
+++ Login for Shimmer/Login_for_ShimmerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_for_ShimmerApp.swift
+//  Login for Shimmer
+//
+//  Created by hosam on 12/30/20.
+//
+
+import SwiftUI
+
+@main
+struct Login_for_ShimmerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
